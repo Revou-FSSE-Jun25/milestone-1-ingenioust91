62a02800c2a4b470e94149c8e92282e430ab7f8b
ingenioust91 <ingecendrawatisalim@gmail.com>
2025-07-23 13:39:09 +0700

    add hamburger menu

--- js/index.js
+++ js/index.js
@@ -1,10 +1,19 @@
 
-  const navbar = document.querySelector('.navigation-bar');
+  // const navbar = document.querySelector('.navigation-bar');
 
-  window.addEventListener('scroll', () => {
-    if (window.scrollY > 50) {
-      navbar.classList.add('solid');
-    } else {
-      navbar.classList.remove('solid');
-    }
-  });
+  // window.addEventListener('scroll', () => {
+  //   if (window.scrollY > 50) {
+  //     navbar.classList.add('solid');
+  //   } else {
+  //     navbar.classList.remove('solid');
+  //   }
+  // });
+
+
+function openNav() {
+  document.getElementById("navigation-mobile").style.visibility = "visible"
+}
+
+function closeNav() {
+  document.getElementById("navigation-mobile").style.visibility = "hidden"
+}
